aaee08665013b521afeda97dac7ba0ab2b4cc429
Othoniel Eduardo Salazar Arenas <othosalazar@gmail.com>
2020-06-29 18:32:32 -0500

    Sendgrid validated V3.5

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,24 +7,55 @@ namespace Composer\Autoload;
 class ComposerStaticInitccaf2d48590752124bcbca280eda665c
 {
     public static $files = array (
-        '3f8bdd3b35094c73a26f0106e3c0f8b2' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/SendGrid.php',
-        '9dda55337a76a24e949fbcc5d905a2c7' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/mail/Mail.php',
+        '79f66bc0a1900f77abe4a9a299057a0a' => __DIR__ . '/..' . '/starkbank/ecdsa/src/ellipticcurve.php',
     );
 
-    public static $prefixesPsr0 = array (
+    public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
-            'SendGrid' => 
-            array (
-                0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
-            ),
+            'SendGrid\\Stats\\' => 15,
+            'SendGrid\\Mail\\' => 14,
+            'SendGrid\\EventWebhook\\' => 22,
+            'SendGrid\\Contacts\\' => 18,
+            'SendGrid\\' => 9,
         ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'SendGrid\\Stats\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/stats',
+        ),
+        'SendGrid\\Mail\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/mail',
+        ),
+        'SendGrid\\EventWebhook\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/eventwebhook',
+        ),
+        'SendGrid\\Contacts\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/contacts',
+        ),
+        'SendGrid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
+        ),
+    );
+
+    public static $classMap = array (
+        'BaseSendGridClientInterface' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/BaseSendGridClientInterface.php',
+        'SendGrid' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/SendGrid.php',
+        'TwilioEmail' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/TwilioEmail.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixesPsr0 = ComposerStaticInitccaf2d48590752124bcbca280eda665c::$prefixesPsr0;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitccaf2d48590752124bcbca280eda665c::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitccaf2d48590752124bcbca280eda665c::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitccaf2d48590752124bcbca280eda665c::$classMap;
 
         }, null, ClassLoader::class);
     }
