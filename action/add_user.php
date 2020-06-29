e312aa83ff1d3d2aaa28286f7d7446e2e3e60ebe
Othoniel Eduardo Salazar Arenas <othosalazar@gmail.com>
2020-06-29 17:41:45 -0500

    Sendgrid invalidated

--- action/add_user.php
+++ action/add_user.php
@@ -108,21 +108,21 @@ if ($rol == 3 || !isset($_SESSION['user_id'])) {
 				$headers .= "Reply-To: golfoticketsupport@golfoticket.com\r\n";
 				$headers .= "Content-type: text/html\r\n";
 
-				// mail($to, $subject, $message, $headers);
+				mail($to, $subject, $message, $headers);
 
-				$from = new SendGrid\Email("Golfo Ticket Support", "golfoticketsupport@golfoticket.com");
-				$subject = "Código de Activación de su cuenta en Golfo Ticket";
-				$to = new SendGrid\Email($name . ' ' . $lastname, $email);
-				$content = new SendGrid\Content("text/plain", $message);
-				$mail = new SendGrid\Mail($from, $subject, $to, $content);
+				// $from = new SendGrid\Email("Golfo Ticket Support", "golfoticketsupport@golfoticket.com");
+				// $subject = "Código de Activación de su cuenta en Golfo Ticket";
+				// $to = new SendGrid\Email($name . ' ' . $lastname, $email);
+				// $content = new SendGrid\Content("text/plain", $message);
+				// $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-				$apiKey = getenv('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');
-				$sg = new \SendGrid($apiKey);
+				// $apiKey = getenv('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');
+				// $sg = new \SendGrid($apiKey);
 
-				$response = $sg->client->mail()->send()->post($mail);
-				echo $response->statusCode();
-				echo $response->headers();
-				echo $response->body();
+				// $response = $sg->client->mail()->send()->post($mail);
+				// echo $response->statusCode();
+				// echo $response->headers();
+				// echo $response->body();
 			} else {
 				$errors[] = "Lo sentimos, algo ha salido mal. Intenta nuevamente." . mysqli_error($con);
 			}
